ae2b9bb3f6cb5f89a1b023fc50e70dc0527c1727
Ayan Bera <138008735+GitAyan007@users.noreply.github.com>
2026-06-20 22:16:04 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s.length() == 1 && s == " ") return true;
+        
+        String clean = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        int left = 0, right = clean.length()-1;
+
+        while(left < right){
+            if(clean.charAt(left) != clean.charAt(right)){
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
